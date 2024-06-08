9bee0735abf028c6047c8352c5c13dcfc297971e
Muhaiminul Islam Ninad <34864492+ninadgns@users.noreply.github.com>
2024-06-09 00:22:44 +0600

    shadcn setup

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,17 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export  function getOrdinalSuffix(position: number) {
-  const suffixes = ["th", "st", "nd", "rd"];
-  const v = position % 100;
-  return suffixes[(v - 20) % 10] || suffixes[v] || suffixes[0];
-}
-export function isSubsequence(seq: string, word: string) {
-  let i = 0;
-  for (let j = 0; j < word.length; j++) {
-      if (seq[i] === word[j]) i++;
-      if (i === seq.length) return true;
-  }
-  return false;
-}
