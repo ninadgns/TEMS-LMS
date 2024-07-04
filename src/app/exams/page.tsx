7d9312fb141b8c9856d533eaa63707843a5ad559
Muhaiminul Islam Ninad <34864492+ninadgns@users.noreply.github.com>
2024-07-04 13:21:19 +0600

    changed database types

--- src/app/exams/page.tsx
+++ src/app/exams/page.tsx
@@ -6,7 +6,7 @@ import ExamForm from './examForm';
 import { ExamInfo } from '@/lib/types';
 import { useState } from 'react';
 const Home: React.FC = () => {
-  const [exam, setExam] = useState<ExamInfo>({ batchName: "", category: "", examDate: new Date(), examFullMark: 10, examTopic: "", subject: "" });
+  const [exam, setExam] = useState<ExamInfo>({ batchName: "", examDate: new Date(), examFullMark: 10, examTopic: "", subject: "" });
 
   const onsubmit = (data: ExamInfo) => {
     setExam(data);
