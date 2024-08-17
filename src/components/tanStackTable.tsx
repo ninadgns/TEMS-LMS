38f3af68226c7dbcef8e7ed496972b6f52a94acf
Muhaiminul Islam Ninad <ninadgns@gmail.com>
2024-08-18 03:13:57 +0600

    Update tanStackTable.tsx

--- src/components/tanStackTable.tsx
+++ src/components/tanStackTable.tsx
@@ -128,24 +128,6 @@ export function DataTable<TData, TValue>({ columns, data, filter }: DataTablePro
 					Next
 				</Button>
 			</div>
-			<div className="flex items-center justify-end space-x-2 py-4">
-				<Button
-					variant="outline"
-					size="sm"
-					onClick={() => table.previousPage()}
-					disabled={!table.getCanPreviousPage()}
-				>
-					Previous
-				</Button>
-				<Button
-					variant="outline"
-					size="sm"
-					onClick={() => table.nextPage()}
-					disabled={!table.getCanNextPage()}
-				>
-					Next
-				</Button>
-			</div>
 		</div>
 	)
 }
